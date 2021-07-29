b70424c692338ab81eefa95ae1fb55353be071b4
程林 <chenglin0475@163.com>
2021-07-29 17:09:29 +0800

    day10_20210729

---
+++ day10/computer.py
@@ -0,0 +1,40 @@
+class comp:
+    pdx = ""
+    jiage = 0
+    cpu = ""
+    neicun = ""
+    daiji = ""
+    def setpdx(self,pdx):
+        self.pdx = pdx
+        pass
+    def getpdx(self):
+        return self.pdx
+    def setjiage(self,jiage):
+        self.jiage = jiage
+        pass
+    def getjiage(self):
+        return self.jiage
+    def setcpu(self,cpu):
+        self.cpu = cpu
+        pass
+    def getcpu(self):
+        return self.cpu
+    def setneicun(self,neicun):
+        self.neicun = neicun
+        pass
+    def getdaiji(self):
+        return self.daiji
+    def setdaiji(self,daiji):
+        self.daiji = daiji
+        pass
+    def getdaiji(self):
+        return self.daiji
+    def dazi(self):
+        print("电脑在打字")
+        pass
+    def palygame(self):
+        print("打游戏")
+        pass
+    def kan(self):
+        print("看视频")
+
---
+++ day10/water.py
@@ -0,0 +1,39 @@
+
+
+
+class watercub:
+    hight = 0
+    rong = 0
+    color = ""
+    cai = ""
+    cun = ""
+    def sethight(self,hight):
+        self.hight = hight
+        pass
+    def gethight(self):
+        return self.hight
+    def setrong(self,rong):
+        self.rong = rong
+        pass
+    def getrong(self):
+        return self.rong
+    def setcolor(self,color):
+        self.color = color
+        pass
+    def getcolor(self):
+        return self.color
+    def setcai(self,cai):
+        self.cai = cai
+        pass
+    def getcai(self):
+        return self.cai
+    def setcun(self,cun):
+        self.cun = cun
+        pass
+    def getcun(self):
+        return self.cun
+
+
+
+
+
